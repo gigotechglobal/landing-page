71a0bc38b3c519e4484f497e5fc62c6e6abd5aba
gospelin <gospelin.gi@gmail.com>
2026-08-22 07:56:13 +0100

    ci: Fix Homepage Test

--- tests/Feature/HomepageTest.php
+++ tests/Feature/HomepageTest.php
@@ -4,5 +4,5 @@ test('homepage loads and contains the hero heading', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
-    $response->assertSee('Transforming Education');
+    $response->assertSee('Transforming \nEducation');
 });
