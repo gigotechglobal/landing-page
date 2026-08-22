9a489c14731a41fc7aa1af147e2356d8e96f198b
gospelin <gospelin.gi@gmail.com>
2026-08-22 06:42:56 +0100

    Add test and workflow

---
+++ tests/Feature/HomepageTest.php
@@ -0,0 +1,8 @@
+<?php
+
+test('homepage loads and contains the hero heading', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(200);
+    $response->assertSee('Transforming Education');
+});
